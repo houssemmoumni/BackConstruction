dae3c34ea34537a7f63be6f9e1587fa43ddb9e14
yosri369 <Mohammedyosri.Selmi@esprit.tn>
2025-02-15 17:05:07 +0100

    making the configuration of the matriel microservice

---
+++ matriel/src/test/java/com/megaminds/matriel/MatrielApplicationTests.java
@@ -0,0 +1,13 @@
+package com.megaminds.matriel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatrielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
