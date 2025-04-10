b127d5313fb77e2a0d0c716db21c96505e10a6c8
FediBenArbi <fedi.benarbi@esprit.tn>
2025-04-10 17:47:41 +0100

    assurance 1.0

--- pointage/src/main/java/com/megaminds/pointage/repositories/HistoriquePointageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.megaminds.pointage.repositories;
-
-import com.megaminds.pointage.entities.HistoriquePointage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HistoriquePointageRepository extends JpaRepository<HistoriquePointage, Long> {
-    // Vous pouvez ajouter des méthodes de recherche personnalisées ici si nécessaire
-}
